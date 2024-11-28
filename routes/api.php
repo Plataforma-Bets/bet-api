2754ec5e7c748a08492d287de67afb13499c32c8
gustavonunes0 <gustavonoroes@outlook.com>
2024-11-28 20:21:41 -0300

    feat: adicionado rotas

--- routes/api.php
+++ routes/api.php
@@ -4,12 +4,22 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\PartidaController;
-
-Route::post('/salvar-partidas', [PartidaController::class, 'store']);
-Route::post('/register', [AuthController::class, 'register']);
-Route::post('/login', [AuthController::class, 'login']);
-
+use App\Http\Controllers\TimeController;
+use App\Http\Controllers\CampeonatosController;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/register', [AuthController::class, 'register']);
+
+Route::get('/ligas', [CampeonatosController::class, 'listarTodasLigas']);
+Route::get('/ligas/pais', [CampeonatosController::class, 'listarLigasPorPais']);
+
+Route::get('/times', [TimeController::class, 'buscarTimes']);
+
+Route::get('/partidas', [PartidaController::class, 'buscarPartidas']);
+Route::get('/partidas/por-time', [PartidaController::class, 'listarPartidasPorTime']);
+
+Route::post('/salvar-logos', [TimeController::class, 'salvarLogos']);
+Route::post('/salvar-partidas', [PartidaController::class, 'store']);
